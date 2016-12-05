73bc139e9ab51030aa98d142f29ca8a279eb602e
jeskert <568361463@qq.com>
2016-12-05 17:31:43 +0800

    controller and scope

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
@@ -11,4 +11,28 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+app.controller('PlayerController', ['$scope', function($scope) {
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = '/media/npr.mp4';
+  $scope.play = function () {
+      $scope.audio.play();
+      $scope.playing = true;
+  };
+  
+  $scope.stop = function () {
+      $scope.audio.pause();
+      $scope.playing = false;
+  };
+
+  $scope.audio.addEventListener('ended', function () {
+      $scope.$apply(function () {
+          $scope.stop()
+      });
+  });
+}]);
+app.controller('RelatedController', ['$scope', function ($scope) {
+    
 }]);
