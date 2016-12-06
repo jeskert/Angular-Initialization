c63358c401531e41cb6d844440152208efa86b90
jeskert <568361463@qq.com>
2016-12-06 15:34:35 +0800

    the music player

--- app/app.js
+++ app/app.js
@@ -13,23 +13,43 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
 
-app.controller('PlayerController', ['$scope', function($scope) {
-  $scope.playing = false;
-  $scope.audio = document.createElement('audio');
-  $scope.audio.src = '/media/npr.mp4';
-  $scope.play = function () {
-      $scope.audio.play();
-      $scope.playing = true;
-  };
-  
-  $scope.stop = function () {
-      $scope.audio.pause();
-      $scope.playing = false;
-  };
+var apiKey = 'MDExODQ2OTg4MDEzNzQ5OTM4Nzg5MzFiZA001',
+    nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
-  $scope.audio.addEventListener('ended', function () {
-      $scope.$apply(function () {
-          $scope.stop()
-      });
-  });
-}]);
+app.controller('PlayerController', function ($scope, $http) {
+
+    var audio = document.createElement('audio');
+    $scope.audio = audio;
+    $scope.play = function (program) {
+        if ($scope.playing){
+            audio.pause();
+        }
+        var url = program.audio[0].format.mp4.$text;
+        audio.src = url;
+        audio.play();
+        $scope.playing = true;
+    }
+    $http({
+        method: 'JSONP',
+        url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+    }).success(function(data, status) {
+        $scope.programs = data.list.story;
+    }).error(function(data, status) {
+    });
+});
+
+app.directive('nprLink', function() {
+    return {
+        restrict: 'EA',
+        require: ['^ngModel'],
+        replace: true,
+        scope: {
+            ngModel: '=',
+            play: '&'
+        },
+        templateUrl: 'view1/nprListItem.html',
+        link: function(scope, ele, attr) {
+            scope.duration = scope.ngModel.audio[0].duration.$text;
+        }
+    }
+});
